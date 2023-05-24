dad070e53565e86aeffb6053eaac17964a304267
thuotracy <tracyjacobs775@gmail.com>
2023-05-24 10:38:47 +0300

    first commit

---
+++ main.py
@@ -0,0 +1,15 @@
+def deposit():
+    while True:
+        amount = input("What would you like to deposit? $")
+        if amount.isdigit():
+            amount = int(amount)
+            if amount > 0:
+                break
+            else:
+                print("Amount must be greater than 0.")
+        else:
+            print("Please enter a number.")        
+
+    return amount    
+
+deposit()    
