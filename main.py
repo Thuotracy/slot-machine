e60e7776abadc6083fe25ff6eb2c9322a6774223
thuotracy <tracyjacobs775@gmail.com>
2023-05-24 11:11:32 +0300

    made chnages

--- main.py
+++ main.py
@@ -49,8 +49,16 @@ def get_bet():
 def main():
     balance = deposit() 
     lines = get_number_of_lines()
-    bet = get_bet()
-    total_bet = bet * lines
+
+    while True:
+        bet = get_bet()
+        total_bet = bet * lines
+
+        if total_bet > balance:
+            print(f"You do not have enough funds to bet that amount. Your current balance is: ${balance}.")
+        else:
+            break
+
     print(f"You are betting ${bet} on {lines} lines. Total bet is ${total_bet}")
 
 
