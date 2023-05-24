e73fb74f05526f45fcd1f72c0f899ccee4c035e0
thuotracy <tracyjacobs775@gmail.com>
2023-05-24 11:29:33 +0300

    created another function]

--- main.py
+++ main.py
@@ -18,6 +18,21 @@ symbol_count = {
 }
 
 
+def get_slot_machine_spin(rows, cols, symbols):
+    all_symbols = []
+    for symbol, symbol_count in symbols.item():
+        for _ in range(symbol_count):
+            all_symbols.append(symbol)
+
+    columns = []
+    for col in range(cols):
+        column = []
+        current_symbols = all_symbols[:]
+        for row in range(rows):
+            value = random.choice(current_symbols)
+            current_symbols.remove(value)
+
+
 def deposit():
     while True:
         amount = input("What would you like to deposit? $")
